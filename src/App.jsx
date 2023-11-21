450ae7c4874b43017cc17237ffedfe8322f5202d
Dita Salsabila <ditsabil@gmail.com>
2023-11-21 15:47:16 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+  // const style = {
+  //   border: '1px solid black',
+  //   width: '630px',
+  //   borderRadius: '12px',
+  // };
+  // const imageStyle = {
+  //   borderRadius: '12px',
+  // };
+
+  // return (
+  //   <div className="menu" style={style}>
+  //     <img
+  //       src="https://img.freepik.com/premium-vector/sanwich-vector-isolated-fast-food_484148-2.jpg"
+  //       alt="sandwich"
+  //       style={imageStyle}
+  //     />
+  //     <p>Rp. 15000</p>
+  //   </div>
+  // );
+}
+
+export default App;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Ini adalah Landing Page</h1>
+      </>
+    );
+  }
+}
